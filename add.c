6feaaa473736c43779aefe405bb44c845aff2fd7
Prikshit Vashisth <136953371+prikshitvashisth@users.noreply.github.com>
2024-04-09 01:10:25 +0530

    add.c

--- add.c
+++ add.c
@@ -3,7 +3,7 @@
  
 int main() {        
     char operator;          
-    double num1, num2, result; 
+    double num1, num2, result;  
       
     // Input the operator and two numbers
     printf("Enter an operator (+, -, *, /): ");
